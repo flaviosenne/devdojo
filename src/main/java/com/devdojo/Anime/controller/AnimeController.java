bdb35b6ff6ed361b753a8d381db16986a5fbdaf9
flaviosenne123 <joaoflausino123@hotmail.com>
2021-05-18 22:22:37 -0300

    aula 8

--- src/main/java/com/devdojo/Anime/controller/AnimeController.java
+++ src/main/java/com/devdojo/Anime/controller/AnimeController.java
@@ -5,7 +5,11 @@ import com.devdojo.Anime.service.AnimeService;
 import com.devdojo.Anime.util.DateUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,8 +26,14 @@ public class AnimeController {
     private final AnimeService animeService;
 
     @GetMapping
-    public List<Anime> list(){
+    public ResponseEntity<List<Anime>> list(){
         log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-        return animeService.listAll();
+        return ResponseEntity.ok(animeService.listAll());
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Anime> findById(@PathVariable Long id){
+        log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
+        return ResponseEntity.ok(animeService.findById(id));
     }
 }
--- src/main/java/com/devdojo/Anime/service/AnimeService.java
+++ src/main/java/com/devdojo/Anime/service/AnimeService.java
@@ -3,7 +3,9 @@ package com.devdojo.Anime.service;
 import com.devdojo.Anime.domain.Anime;
 import com.devdojo.Anime.repository.AnimeRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -11,9 +13,17 @@ import java.util.List;
 @RequiredArgsConstructor
 public class AnimeService {
 
+    private List<Anime> animes = List.of(new Anime(1L,"Naruto"), new Anime(2L,"CDZ"));
 //    private final AnimeRepository animeRepository;
 
     public List<Anime> listAll(){
-        return List.of(new Anime(1L,"Naruto"), new Anime(2L,"CDZ"));
+        return animes;
+    }
+
+    public Anime findById(Long id){
+        return animes.stream()
+                .filter(anime -> anime.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not found"));
     }
 }
