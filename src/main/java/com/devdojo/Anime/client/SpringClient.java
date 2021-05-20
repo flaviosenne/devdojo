6f606a4a5d94e072e5c601623dc9e8e739cf0ed5
flaviosenne123 <joaoflausino123@hotmail.com>
2021-05-20 18:58:34 -0300

    aula 28 post request in back like client front-end

--- src/main/java/com/devdojo/Anime/client/SpringClient.java
+++ src/main/java/com/devdojo/Anime/client/SpringClient.java
@@ -3,6 +3,7 @@ package com.devdojo.Anime.client;
 import com.devdojo.Anime.domain.Anime;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
@@ -13,8 +14,8 @@ import java.util.List;
 @Log4j2
 public class SpringClient {
     public static void main(String[] args){
-//        ResponseEntity<Anime> entity = new RestTemplate().getForEntity("http://localhost:81/animes", Anime.class, 2);
-//        log.info(entity);
+        ResponseEntity<Anime> entity = new RestTemplate().getForEntity("http://localhost:81/animes/1", Anime.class, 2);
+        log.info(entity);
 
         Anime[] animes = new RestTemplate().getForObject("http://localhost:81/animes", Anime[].class);
         log.info(Arrays.toString(animes));
@@ -24,5 +25,22 @@ public class SpringClient {
                 }
         );
         log.info(listAnime.getBody());
+
+        // requisição ao back-end
+        //o primeiro parametro é a URI, depois o objeto que deseja enviar e por fim qual o tipo de retorno
+        Anime kingdom = Anime.builder().name("kingdom").build();
+        Anime animeSaved = new RestTemplate().postForObject("http://localhost:81/animes",
+                kingdom, Anime.class);
+
+        log.info(animeSaved);
+
+        Anime samurai = Anime.builder().name("samurai").build();
+        ResponseEntity<Anime> samuraiSaved = new RestTemplate().exchange("http://localhost:81/animes",
+                HttpMethod.POST,
+                new HttpEntity<>(samurai),
+                new ParameterizedTypeReference<Anime>() { }
+        );
+
+        log.info(samuraiSaved);
     }
 }
